c3a59ae79c8f3cc87bbded5b0527747866137611
pael1 <rafaelfernandez677@gmail.com>
2022-06-03 15:41:04 +0800

    test

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -51,7 +51,7 @@ const Skills = () => {
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Csharp} alt="HTML icon" />
-                  <p className='my-4'>AWS</p>
+                  <p className='my-4'>AWSs</p>
               </div>
           </div>
       </div>
