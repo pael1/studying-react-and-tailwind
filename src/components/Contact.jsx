6cdcb2dfc9f0dc945b8c1a6828c06b395a74d696
pael1 <rafaelfernandez677@gmail.com>
2022-06-06 11:06:01 +0800

    contact updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,11 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-            </div>
+                  </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 my-8 mx-auto flex items-center'>Send</button>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
         </form>
     </div>
   )
